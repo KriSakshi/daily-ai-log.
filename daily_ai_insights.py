614f62db53e0dafe3a27d31fdf926314106213d1
Kumari Sakshi <krisakshi015@gmail.com>
2025-10-23 02:32:45 +0530

    Update daily_ai_insights.py

--- daily_ai_insights.py
+++ daily_ai_insights.py
@@ -15,6 +15,7 @@ insights = [
     "Quick Tip: Dropout layers help prevent overfitting in neural networks.",
     "AI Fact: Transformers allow models to focus on relevant parts of sequences."
     "AI Fact : The Healthcare AI Imaging Market to Exceed $7 Billion by 2026"
+    "AI Fact:AI in the Insurance Sector to Trim Claims Processing Time by 70%."
 ]
 
 # Pick a random insight
